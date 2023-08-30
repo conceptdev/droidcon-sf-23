6dff8837d1bc8766b889f7037f91e06693fdb59d
conceptd3v <130496214+conceptd3v@users.noreply.github.com>
2023-08-30 14:27:23 -0700

    add link to possible Java tokenizer implementation

--- Jetchat/app/src/main/java/com/example/compose/jetchat/data/Tokenizer.kt
+++ Jetchat/app/src/main/java/com/example/compose/jetchat/data/Tokenizer.kt
@@ -1,8 +1,15 @@
 package com.example.compose.jetchat.data
 
 /**
+ * Count tokens so that we can understand how close we are
+ * to the model's maximum input token limit
  *
  * https://community.openai.com/t/what-is-the-openai-algorithm-to-calculate-tokens/58237
+ *
+ * TODO: investigate this tool
+ * https://github.com/knuddelsgmbh/jtokkit
+ * linked from
+ * https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
  */
 class Tokenizer {
     companion object {
